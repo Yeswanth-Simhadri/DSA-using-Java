7278b461f9a4d0e487664fb29f4800ad0353474c
yeswanth simhadri <yeswanth1704@gmail.com>
2025-04-03 21:49:52 +0530

    added Kth level

--- BinarayTree/ClassRoom.java
+++ BinarayTree/ClassRoom.java
@@ -76,6 +76,18 @@ public class ClassRoom {
             System.out.print(temp.data+" ");
         }
     }
+    public static  void Klevel(Node root,int level,int k){
+        
+        if(root==null){
+            return;
+        }
+        if(level==k){
+            System.out.print(root.data+" ");
+            return;
+        } 
+        Klevel(root.left,level+1,k);
+        Klevel(root.right,level+1,k);
+    }
         
     public static void main(String[] args) {
         Node root =new Node(1);
@@ -86,6 +98,7 @@ public class ClassRoom {
         root.right.left=new Node(6);
         root.right.right=new Node(7);
         // System.out.println(diameter(root).dia);
-        topView(root);
+        // topView(root);
+        Klevel(root,1,3);
     }
 }
